e5461e2acc3ab32662e41338cbb68c211fbca1e4
微笑 <wang1234561211@outlook.com>
2023-07-13 22:40:51 +0800

    feat: 新增第一版本的项目扫描逻辑

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,7 @@
 )]
 
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::env;
 
 #[tauri::command]
 fn on_button_clicked() -> String {
@@ -15,9 +16,39 @@ fn on_button_clicked() -> String {
     format!("on_button_clicked called from Rust! (timestamp: {since_the_epoch}ms)")
 }
 
+// 扫描home目录下的所有包含.git的目录
+
+
+#[tauri::command]
+fn scan_directory() -> Vec<String> {
+    let path = std::env::var("HOME").unwrap_or_else(|_| "".into());
+    let mut result = Vec::new();
+    for entry in walkdir::WalkDir::new(&path) {
+        match entry {
+            Ok(entry) => {
+                let path = entry.path();
+                if path.ends_with(".git") {
+                    if let Some(parent_path) = path.parent() {
+                        if let Some(parent_path_str) = parent_path.to_str() {
+                            result.push(parent_path_str.to_owned());
+                        }
+                    }
+                }
+            },
+            Err(_err) => {
+                // 如果出现错误，我们只是打印一个警告，而不是让整个程序崩溃
+                eprintln!("Warning: could not access a path");
+            }
+        }
+    }
+    result
+}
+
+
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![on_button_clicked])
+        .invoke_handler(tauri::generate_handler![on_button_clicked, scan_directory])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
